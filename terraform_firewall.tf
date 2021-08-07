24254d950dc06e00e4468f019fc59a3376f1f208
Florian Kaiser <florian.kaiser@fnkr.net>
2021-08-07 15:26:23 +0200

    Remove dummy firewall rule

--- terraform_firewall.tf
+++ terraform_firewall.tf
@@ -1,16 +1,6 @@
 resource "hcloud_firewall" "nodefw" {
   name = "${var.cluster_name}-node"
 
-  # Add fake rule to invert default policy from allow to drop
-  rule {
-    direction = "in"
-    protocol  = "icmp"
-    source_ips = [
-      "0.0.0.0/32",
-      "::/128"
-    ]
-  }
-
   # Allow access from this machine
   rule {
     direction  = "in"
