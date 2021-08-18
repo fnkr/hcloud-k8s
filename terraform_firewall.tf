e8944f5911d06b3fdb5a0738cf1d0523fb5456e5
Florian Kaiser <florian.kaiser@fnkr.net>
2021-08-18 16:13:00 +0200

    Allow access to Kubernetes API from local machine
    Useful if there are no control node load balancers.

--- terraform_firewall.tf
+++ terraform_firewall.tf
@@ -1,7 +1,8 @@
 resource "hcloud_firewall" "nodefw" {
   name = "${var.cluster_name}-node"
 
-  # Allow access from this machine
+  # Allow access from this machine only
+
   rule {
     direction  = "in"
     protocol   = "icmp"
@@ -12,6 +13,13 @@ resource "hcloud_firewall" "nodefw" {
     direction  = "in"
     protocol   = "tcp"
     port       = "22"
+    source_ips = ["${data.http.my_ip_v4_address.body}/32"]
+  }
+
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "6443"
     source_ips = ["${data.http.my_ip_v4_address.body}/32"]
   }
 }
