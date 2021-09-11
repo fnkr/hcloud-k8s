8dabb79a95453d6077e75bdfbc47519c18aa2aa5
Florian Kaiser <florian.kaiser@fnkr.net>
2021-09-11 17:54:29 +0200

    Make names shorter when using FQDNs

--- terraform_locals.tf
+++ terraform_locals.tf
@@ -20,13 +20,13 @@ locals {
     (var.initializer_label_key) : var.initializer_label_value,
   }
 
-  cluster_resource_number_format = "%03d"
+  cluster_resource_number_format = var.cluster_domain == "" ? "%03d" : "%d"
   cluster_resource_name_short    = "${var.cluster_name}-%s-${local.cluster_resource_number_format}"
   cluster_resource_name_fqdn     = "%s-${local.cluster_resource_number_format}.${var.cluster_domain}"
   cluster_resource_name          = var.cluster_domain == "" ? local.cluster_resource_name_short : local.cluster_resource_name_fqdn
 
-  cluster_resource_name_controlnode = var.cluster_domain == "" ? "control" : "controlnode"
-  cluster_resource_name_controllb   = var.cluster_domain == "" ? "control" : "controllb"
-  cluster_resource_name_workernode  = var.cluster_domain == "" ? "worker" : "workernode"
-  cluster_resource_name_workerlb    = var.cluster_domain == "" ? "worker" : "workerlb"
+  cluster_resource_name_controlnode = var.cluster_domain == "" ? "control" : "cn"
+  cluster_resource_name_controllb   = var.cluster_domain == "" ? "control" : "cl"
+  cluster_resource_name_workernode  = var.cluster_domain == "" ? "worker" : "wn"
+  cluster_resource_name_workerlb    = var.cluster_domain == "" ? "worker" : "wl"
 }
